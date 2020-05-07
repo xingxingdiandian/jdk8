4dc23d9d831674678076adde5df66a7b905236fe
lishang <shangli@isoftstone.com>
2020-05-07 17:13:32 +0800

    函数式接口

---
+++ src/com/demo1/FunctionalInterface/Demo.java
@@ -0,0 +1,37 @@
+package com.demo1.FunctionalInterface;
+
+/**
+ * @author shangli
+ * @description:
+ * TODO 函数式接口的使用：
+ * 一般可以作为方法的参数和返回值类型
+ *
+ * @date create in 2020/5/6 11:32
+ */
+public class Demo {
+    //定义一个方法，参数使用函数式接口MyFunctionalInterface
+    public static void show(MyFunctionalInterface myInter) {
+        myInter.method();
+    }
+
+    public static void main(String[] args) {
+        //1.参数使用实现类
+        show(new MyFunctionalInterfaceImpl());
+
+        //2.参数使用匿名内部类//匿名内部类会生成class文件
+        show(new MyFunctionalInterface() {
+            @Override
+            public void method() {
+                System.out.println("使用匿名内部类重写接口中的抽象方法");
+            }
+        });
+
+        //3.上一方法的匿名内部类使用lambda表达式//使用lambda和匿名内部类底层原理不同
+        show(()->{
+            System.out.println("使用lambda表达式重写接口中的抽象方法");
+        });
+
+        //4.对上一方法进行简化
+        show(()->System.out.println("使用lambda表达式重写接口中的抽象方法"));
+    }
+}
---
+++ src/com/demo1/FunctionalInterface/MyFunctionalInterface.java
@@ -0,0 +1,12 @@
+package com.demo1.FunctionalInterface;
+
+/**
+ * @author shangli
+ * @description:
+ * TODO 函数式接口：有且只有一个抽象方法的接口
+ * @date create in 2020/5/6 11:27
+ */
+@FunctionalInterface
+public interface MyFunctionalInterface {
+    public abstract void method();
+}
